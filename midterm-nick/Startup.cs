d70701fbbe748b35c139c2117218c7f97726f655
NicKuznecov <NicKuznecov@gmail.com>
2015-07-02 20:40:50 +0000

    initial

---
+++ midterm-nick/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(midterm_nick.Startup))]
+namespace midterm_nick
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
